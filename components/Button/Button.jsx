391afc0b72a971618254d662bd88b5cb003ac5b1
Sajeesh007 <sajeesh8112001@gmail.com>
2023-02-08 16:38:03 +0530

    ready to merge to main

---
+++ components/Button/Button.jsx
@@ -0,0 +1,13 @@
+
+export default function Button({value}) {
+  return (
+    <button className='
+		bg-gradient-to-r from-fuchsia-600 to-violet-600 
+		px-4 py-2 
+		rounded-lg
+		font-bold
+	'>
+		{value}
+    </button>
+  )
+}
---
+++ pages/edit/[designId].jsx
@@ -0,0 +1,11 @@
+import Topbar from "@/model/Editing/Topbar/Topbar";
+
+export default function EditingInterface() {
+  return (
+    <div className='w-screen h-screen flex flex-col px-4'>
+        
+        <Topbar/>
+
+    </div>
+  )
+}
