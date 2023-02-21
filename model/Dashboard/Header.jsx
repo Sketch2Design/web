12f85f9e6136b98b33a3586ddad17915d5e98b67
Anjana Manoj <anjanamanoj117@gmail.com>
2023-02-21 14:48:32 +0530

    ubu

--- model/Dashboard/Header.jsx
+++ model/Dashboard/Header.jsx
@@ -3,7 +3,7 @@ import Button from '@/components/Button/Button'
 
 export default function Header() {
   return (
-    <div className='flex justify-between items-center w-full h-20 px-6 py-6'>
+    <div className='flex justify-between items-center w-full h-20 px-14 py-6 pt-5'>
         <p>Hello Name</p>
         <Button value='Create a design'/>
     </div>
--- model/Workspace/Sidebar/Sidebar.jsx
+++ model/Workspace/Sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
-import {AiFillHome,AiFillLayout,AiFillSetting,AiOutlineLogout} from 'react-icons/ai'
+import {AiFillFolder, AiFillHome,AiFillLayout,AiFillSetting,AiOutlineLogout} from 'react-icons/ai'
 import Button from '@/components/Button/Button'
 import SidebarItems from './SidebarItems'
+import Link from 'next/link'
 
 export default function Side() {
   return (
-    <div className='bg-zinc-800 h-screen w-96 rounded-tr-xl rounded-br-xl flex flex-col items-center'>
+    <div className='bg-zinc-800 h-full fixed w-96 rounded-tr-xl rounded-br-xl flex flex-col items-center'>
         <p className='font-bold text-4xl flex justify-center py-10'>Sketch</p>
 
         <div className='flex flex-col justify-center space-y-2 mt-10 mb-16'>
@@ -15,10 +16,11 @@ export default function Side() {
 
         <div className='flex flex-col justify-center space-y-4'>
              <SidebarItems icon={<AiFillHome/>} value='Dashboard'/>
+             <Link href='/template'>
              <SidebarItems icon={<AiFillLayout/>} value='Templates'/>
+             </Link>
              <SidebarItems icon={<AiFillSetting/>} value='Settings'/>
-        </div>
-        <div className='mt-64'>
+             <SidebarItems icon={<AiFillFolder/>} value='My designs'/>
               <SidebarItems icon={<AiOutlineLogout/>} value='Logout'/>
         </div>
     </div>
