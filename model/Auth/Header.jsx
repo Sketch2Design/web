9f651665c68c5984616b1b34ffe984ee8c2e2e7c
theertha11 <theertham2k@gmail.com>
2023-02-28 15:24:55 +0530

    auth witout functionality

---
+++ model/Auth/Header.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Header(){
+    return(
+        <div className=" h-20 bg-zinc-900 text-white flex justify-between items-center px-6">
+            <h3 className="font-bold">Sketch</h3>
+            <div className="flex space-x-5 items-center">
+                <Link href="/login"><h5>Sign in</h5></Link>
+                <h5>Sign up</h5>
+            </div>
+        </div>
+    )
+}
